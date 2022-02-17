986b3a3736f54bfb95feb5ab6cf379c19eced4f5
Chaoneng Tan <chaonengtan@Chaonengs-MacBook-Air.local>
2022-02-17 13:08:47 -0800

    exersize1

--- tests/test_carbon_dating.py
+++ tests/test_carbon_dating.py
@@ -1,7 +1,7 @@
 import unittest
 import pytest
 import math
-from ..carbon_dating import get_age_carbon_14_dating
+from carbon_dating import get_age_carbon_14_dating
 
 # Write a unit test which feed 0.35 to the function.
 # The result should be '8680.34'. Does the function handles
@@ -10,3 +10,15 @@ from ..carbon_dating import get_age_carbon_14_dating
 # Add the necessary logic to make sure the function handle
 # every possible input properly. Then write a unit test againt
 # this special case.
+
+def testCarbonDating():
+    carbon14ratio=.35
+    # check if 0 or negative
+    if carbon14ratio <= 0:
+        assert False
+        return
+    # check if not int or float
+    if type(carbon14ratio) != 'int' or type(carbon14ratio) != 'float':
+        assert False
+        return
+    assert math.isclose(get_age_carbon_14_dating(carbon14ratio), 8680.34, abs_tol=0.05)
